5fd053905d3c815def875b57c9f422ac8ed0d206
Eranyan Ravanan <eravanan@mbs.education>
2025-02-11 11:46:37 +0100

    Remove redundant files

--- LED_Controller.h
+++
@@ -1,9 +0,0 @@
-#ifndef LED_CONTROLLER_H
-#define LED_CONTROLLER_H
-
-#include <stdbool.h>
-
-void LED_Controller_Init(void);
-void LED_Controller_Update(float temperature);
-
-#endif // LED_CONTROLLER_H#pragma once
--- temperature_sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEMPERATURE_SENSOR_H
-#define TEMPERATURE_SENSOR_H
-
-#include <stdint.h>
-
-void TemperatureSensor_Init(void);
-float TemperatureSensor_Read(uint8_t revision);
-
-#endif // TEMPERATURE_SENSOR_H
